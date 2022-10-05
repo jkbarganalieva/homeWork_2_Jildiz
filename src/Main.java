719e40cc92622f013a93afa49134a4d288e60097
barganalieva <jkbarganalieva@gmail.com>
2022-10-05 06:51:29 +0300

    change some code

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,42 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-    recommendation(50,28);
-    recommendation(37,20);
-    recommendation(16,-5);
-    recommendation(13,35);
-    recommendation(54,-2);
-    recommendation(generateRandomAge(), 10);
-    recommendation(generateRandomAge(),generateRandomTemperature());
+        System.out.println(recommendation(50,28));
+        System.out.println(recommendation(37,20));
+        System.out.println(recommendation(16,-5));
+        System.out.println(recommendation(13,35));
+        System.out.println(recommendation(54,-2));
+        System.out.println(recommendation(generateRandomAge(), 10));
+        System.out.println(recommendation(generateRandomAge(),generateRandomTemperature()));
 
-        //System.out.println(generateRandomTemperature());
+
     }
 
 
-    public static int recommendation(int ageOfPerson, int temperatureOnTheStreet) {
+    public static String recommendation(int ageOfPerson, int temperatureOnTheStreet) {
         // возвращаемый метод с входящими параметрами
-        if (ageOfPerson > 20 && ageOfPerson<45 && temperatureOnTheStreet>-20 &&temperatureOnTheStreet<30){
+        if (ageOfPerson > 20 && ageOfPerson<45 && temperatureOnTheStreet>-20 &&temperatureOnTheStreet<30)
             //System.out.println("You can go for a walk");
-            System.out.println(goForAWalk());
-        }else if (ageOfPerson<20 && temperatureOnTheStreet>0 && temperatureOnTheStreet<28){
-            System.out.println(goForAWalk());
+            //System.out.println(goForAWalk());
+            return goForAWalk();
+            //return "You can go look";
+        else if (ageOfPerson<20 && temperatureOnTheStreet>0 && temperatureOnTheStreet<28)
+            //System.out.println(goForAWalk());
+            return goForAWalk();
+            //return "you can go look";
+        else if (ageOfPerson>45 && temperatureOnTheStreet>-10 && temperatureOnTheStreet<25)
+            //System.out.println(goForAWalk());
+            return goForAWalk();
+            //return "you can go look";
+
+        else
+            //System.out.println("stay at home");
+            return "stay at home";
         }
 
-        else if (ageOfPerson>45 && temperatureOnTheStreet>-10 && temperatureOnTheStreet<25){
-            System.out.println(goForAWalk());
-        }
-        else{
-            System.out.println("stay at home");
-        }
+        //return temperatureOnTheStreet;
 
-        return temperatureOnTheStreet;
 
-    }
 
     public static String goForAWalk() {
         // возвращаемый метод без входящих параметров
@@ -47,7 +52,9 @@ public class Main {
         System.out.println("random number of age: "+ a);
         return a;
 
-    }public static int generateRandomTemperature(){
+    }
+
+    public static int generateRandomTemperature(){
         //int b = (int) (Math.random()*(200+1)) - 100;
         Random random = new Random();
         int b = random.nextInt(50)-30;
@@ -56,7 +63,7 @@ public class Main {
 
     }
 
-    }
+}
 
 
 
